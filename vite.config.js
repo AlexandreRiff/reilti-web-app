080b0fe005d3eea720dd58b26c48b510183a3dd3
Alexandre Riff da Costa <alexandre.rdcosta@gmail.com>
2023-10-13 23:52:25 -0300

    fix could not resolve entry module "resources/css/login.css"

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/css/login.css",
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
