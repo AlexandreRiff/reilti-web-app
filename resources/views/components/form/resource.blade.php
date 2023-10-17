471143f12433948e86d22c03e5dd9f7c39abcba6
Alexandre Riff da Costa <alexandre.rdcosta@gmail.com>
2023-10-17 14:36:53 -0300

    add validation feedback

--- resources/views/components/form/resource.blade.php
+++ resources/views/components/form/resource.blade.php
@@ -3,6 +3,7 @@
 <form action="{{ $action }}" method="POST" enctype="multipart/form-data" novalidate class="p-3">
     @csrf()
     @method($method)
+
     <!-- TITULO -->
     <div class="mb-3">
         <label for="add__title" class="form-label add__label">Título *</label>
@@ -23,38 +24,61 @@
     <div class="mb-3">
         <div class="mb-3">
             <label for="add__tech-area" class="form-label add__label">Área tecnológica</label>
-            <input type="text" name="techArea" id="add__tech-area" class="form-control fw-light"
+            <input type="text" name="techArea" id="add__tech-area"
+                class="form-control fw-light @error('techArea') is-invalid @enderror"
                 @if (old('techArea')) value="{{ old('techArea') }}"
                     @elseif (isset($resource->techAreas))
                         @foreach ($resource->techAreas as $techArea)
                             value="{{ $techArea->name }}"
                         @endforeach @endif />
+
+            @error('techArea')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+
         </div>
     </div>
     <!-- END AREA TECNOLOGICA -->
 
-
     <!-- CURSO -->
     <div class="mb-3">
         <label for="add__course" class="form-label add__label">Curso</label>
-        <input type="text" name="course" id="add__course" class="form-control fw-light"
+        <input type="text" name="course" id="add__course"
+            class="form-control fw-light @error('course') is-invalid @enderror"
             @if (old('course')) value="{{ old('course') }}"
             @elseif (isset($resource->courses))
                 @foreach ($resource->courses as $course)
                     value="{{ $course->name }}"
                 @endforeach @endif />
+
+        @error('course')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+
     </div>
     <!-- END CURSO -->
 
     <!-- DISCIPLINA -->
     <div class="mb-3">
         <label for="add__discipline" class="form-label add__label">Disciplina</label>
-        <input type="text" name="discipline" id="add__discipline" class="form-control fw-light"
+        <input type="text" name="discipline" id="add__discipline"
+            class="form-control fw-light @error('discipline') is-invalid @enderror"
             @if (old('discipline')) value="{{ old('discipline') }}"
             @elseif (isset($resource->disciplines))
                 @foreach ($resource->disciplines as $discipline)
                     value="{{ $discipline->name }}"
                 @endforeach @endif />
+
+        @error('discipline')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+
     </div>
     <!-- END DISCIPLINA -->
 
@@ -84,7 +108,15 @@
     <!-- DESCRICAO -->
     <div class="mb-3">
         <label for="add__description" class="form-label add__label">Descrição</label>
-        <textarea rows="6" name="description" id="add__description" class="form-control fw-light"> {{ old('description', $resource->description ?? '') }}</textarea>
+        <textarea rows="6" name="description" id="add__description"
+            class="form-control fw-light @error('description') is-invalid @enderror"> {{ old('description', $resource->description ?? '') }}</textarea>
+
+        @error('description')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+
     </div>
     <!-- END DESCRICAO -->
 
@@ -116,13 +148,21 @@
     <div class="mb-3">
         <label for="add__file" class="form-label add__label">Upload *</label>
         <input type="file" accept=".zip, .pdf, audio/*" name="file" required id="add__file"
-            class="form-control fw-light @error('file') is-invalid @enderror" data-js="inputFile" />
+            class="form-control fw-light @error('file') is-invalid @enderror @error('fileInitial') is-invalid @enderror"
+            data-js="inputFile" />
 
         @error('file')
             <div class="invalid-feedback">
                 {{ $message }}
             </div>
         @enderror
+
+        @error('fileInitial')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+
     </div>
     <!-- END UPLOAD -->
 
@@ -131,30 +171,35 @@
         <p class="mb-2 add__label">Selecione o arquivo de inicialização *</p>
         <div class="p-3 border border-1 rounded-2 fs-09" data-js="directoryTree"></div>
         <input type="hidden" name="fileInitial" value="" data-js="inputFileInitial" />
-        @error('fileInitial')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-        @enderror
     </div>
     <!-- END DIRECTORY TREE -->
 
     <!-- TAGS -->
     <div class="mb-3">
         <label for="add__tags" class="form-label add__label">Tags</label>
-        <input type="text" name="tags" id="add__tags" class="form-control fw-light"
+        <input type="text" name="tags" id="add__tags"
+            class="form-control fw-light @error('tags') is-invalid @enderror"
             @if (old('tags')) value="{{ old('tags') }}"
             @elseif (isset($resource->tags))
                 @foreach ($resource->tags as $tag)
                     value="{{ $tag->name }}"
                 @endforeach @endif />
+
+        @error('tags')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+
     </div>
     <!-- END TAGS -->
 
     <!-- IMAGEM DE DESTAQUE -->
     <div class="mb-3">
         <label for="add__image" class="form-label add__label">Imagem de destaque</label>
-        <label for="add__image" class="d-block p-5 rounded-2 add-image__box" data-js="boxImageDragAndDrop">
+        <label for="add__image" class="d-block p-5 rounded-2 add-image__box @error('image') is-invalid @enderror"
+            style="@error('image') border-color: var(--bs-form-invalid-border-color) @enderror"
+            data-js="boxImageDragAndDrop">
             <input type="file" accept="image/*" name="image" id="add__image" class="d-none"
                 data-js="inputAddImage" />
             <img src="{{ isset($resource->thumbnail) ? Storage::url($resource->thumbnail) : '' }}"
@@ -177,6 +222,12 @@
                 </span>
             </p>
         </label>
+
+        @error('image')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
     </div>
     <!-- EMD IMAGEM DE DESTAQUE -->
 
